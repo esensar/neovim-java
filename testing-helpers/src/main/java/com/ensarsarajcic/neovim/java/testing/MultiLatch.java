3ba07c51c838a953711a94b3f6caadcfa69a9004
esensar <es.ensar@gmail.com>
2018-06-14 10:49:19 +0200

    Clean up BackgroundRPCListenerTest

---
+++ testing-helpers/src/main/java/com/ensarsarajcic/neovim/java/testing/MultiLatch.java
@@ -0,0 +1,52 @@
+/*
+ * MIT License
+ *
+ * Copyright (c) 2018 Ensar Sarajčić
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+package com.ensarsarajcic.neovim.java.testing;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.atomic.AtomicInteger;
+
+public final class MultiLatch {
+
+    private List<CountDownLatch> countDownLatches = new ArrayList<>();
+
+    private AtomicInteger awaitIndex = new AtomicInteger(0);
+    private AtomicInteger countDownIndex = new AtomicInteger(0);
+
+    public MultiLatch(int latches) {
+        for (int i = 0; i < latches; i++) {
+            countDownLatches.add(new CountDownLatch(1));
+        }
+    }
+
+    public synchronized void await() throws InterruptedException {
+        countDownLatches.get(awaitIndex.getAndIncrement()).await();
+    }
+
+    public synchronized void countDown() {
+        countDownLatches.get(countDownIndex.getAndIncrement()).countDown();
+    }
+}
