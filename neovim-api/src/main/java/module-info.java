8a32db5dcac256d8730f444de3effcb51fb3ed55
esensar <es.ensar@gmail.com>
2018-07-12 14:08:45 +0200

    Add module info to neovim api

---
+++ neovim-api/src/main/java/module-info.java
@@ -0,0 +1,9 @@
+module neovimapi {
+    requires corerpc;
+    requires reactivecorerpc;
+    requires jackson.annotations;
+    requires msgpack.core;
+    requires jackson.core;
+    requires jackson.databind;
+    requires jackson.dataformat.msgpack;
+}
