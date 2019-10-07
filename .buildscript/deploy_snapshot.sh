7d69e2040595fa663818726c1a398b06e9522e96
Ensar Sarajčić <es.ensar@gmail.com>
2019-10-07 23:23:22 +0200

    Migrate to JDK 13
     * Known issue: some tests fail due to Mockito module issues

--- .buildscript/deploy_snapshot.sh
+++ .buildscript/deploy_snapshot.sh
@@ -6,7 +6,7 @@
 # https://benlimmer.com/2013/12/26/automatically-publish-javadoc-to-gh-pages-with-travis-ci/
 
 SLUG="esensar/neovim-java"
-JDK="openjdk11"
+JDK="openjdk13"
 BRANCH="master"
 
 set -e
