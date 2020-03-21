5b94ac5c26bc92b025a90e2a86cc6178b42646da
Ensar Sarajčić <es.ensar@gmail.com>
2020-03-21 16:46:40 +0100

    Upload deploy script to upload to github too

--- .buildscript/deploy.sh
+++ .buildscript/deploy.sh
@@ -2,6 +2,9 @@
 
 # Deploy and sign a jar, source jar and javadoc jar to Sonatype's staging repo. After this it has to be released on https://oss.sonatype.org/#stagingRepositories
 # It requires proper GPG signing configuration in local maven settings
+
+# Also deploys to github
+# Requires GH_TOKEN and github profile set up
 key="$1"
 
 case $key in
@@ -29,6 +32,7 @@ echo "Removing SNAPSHOT from version"
 mvn versions:set -DremoveSnapshot
 echo "Deploying to staging."
 mvn clean source:jar javadoc:jar verify gpg:sign deploy -DskipTests -P release
+mvn deploy -DaltDeploymentRepository=github::default::https://maven.pkg.github.com/esensar/neovim-java -Dtoken=$GH_TOKEN -P github
 echo "Deployed to staging. Visit https://oss.sonatype.org/#stagingRepositories to release."
 echo "Changing version to $NEXT_VERSION-SNAPSHOT. Make sure to commit this"
 mvn versions:set -DnewVersion=$NEXT_VERSION-SNAPSHOT
