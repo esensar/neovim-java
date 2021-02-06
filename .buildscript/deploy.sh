7b68f067b5adf8214ca677c31de85de4bfa68b7d
Ensar Sarajčić <es.ensar@gmail.com>
2021-02-06 19:59:00 +0100

    Version 0.2.1 release

--- .buildscript/deploy.sh
+++ .buildscript/deploy.sh
@@ -32,7 +32,7 @@ echo "Removing SNAPSHOT from version"
 mvn versions:set -DremoveSnapshot
 echo "Deploying to staging."
 mvn clean source:jar javadoc:jar verify gpg:sign deploy -DskipTests -P release
-mvn deploy -DaltDeploymentRepository=github::default::https://maven.pkg.github.com/esensar/neovim-java -Dtoken=$GH_TOKEN -P github
+# mvn deploy -DaltDeploymentRepository=github::default::https://maven.pkg.github.com/esensar/neovim-java -Dtoken=$GH_TOKEN -P github
 echo "Deployed to staging. Visit https://oss.sonatype.org/#stagingRepositories to release."
 echo "Changing version to $NEXT_VERSION-SNAPSHOT. Make sure to commit this"
 mvn versions:set -DnewVersion=$NEXT_VERSION-SNAPSHOT
